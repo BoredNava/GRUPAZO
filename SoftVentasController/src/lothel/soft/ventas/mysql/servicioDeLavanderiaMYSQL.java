6d5f0366c827af7a50928642f547703487de5a8d
a20203869 <a20203869@pucp.edu.pe>
2024-04-15 22:05:10 -0500

    version 6 andres

---
+++ SoftVentasController/src/lothel/soft/ventas/mysql/servicioDeLavanderiaMYSQL.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package lothel.soft.ventas.mysql;
+
+/**
+ *
+ * @author efeproceres
+ */
+public class servicioDeLavanderiaMYSQL {
+
+}
